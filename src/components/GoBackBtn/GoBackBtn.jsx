3197703ebd0f88f48553bc4acf989ca6d40f9f30
Veronika Kreinina <veronika.kr.0608@gmail.com>
2024-11-23 11:47:04 +0100

    add btn

--- src/components/GoBackBtn/GoBackBtn.jsx
+++ src/components/GoBackBtn/GoBackBtn.jsx
@@ -1,3 +1,10 @@
-export const GoBackBtn = () => {
-  return <h3>GoBackBtn</h3>;
+import { Link } from 'react-router-dom';
+import s from './GoBackBtn.module.css';
+
+export const GoBackBtn = ({ path, children }) => {
+  return (
+    <Link to={path} className={s.link}>
+      {children}
+    </Link>
+  );
 };
